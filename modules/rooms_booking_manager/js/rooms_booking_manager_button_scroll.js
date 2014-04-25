035eac7e970fcf7f90b27f1725e856812d9d3473
Primoz Susa <primoz.susa@gmail.com>
2014-04-25 20:20:26 +0200

    Current search info and Place booking fixed scroll follow.

--- modules/rooms_booking_manager/js/rooms_booking_manager_button_scroll.js
+++ modules/rooms_booking_manager/js/rooms_booking_manager_button_scroll.js
@@ -1,7 +1,7 @@
 (function ($) {
   $(document).ready(function () {
-    if ($('.booking_legend').offset() != null) {
-      var top = $('.booking_legend').offset().top - parseFloat($('.booking_legend').css('marginTop').replace(/auto/,0));
+    if ($('.rooms-current-search__info').offset() != null) {
+      var top = $('.rooms-current-search__info').offset().top - parseFloat($('.rooms-current-search__info').css('marginTop').replace(/auto/,0));
     }
 
     $(window).scroll(function () {
@@ -10,8 +10,8 @@
   });
 
   $(document).ready(function () {
-    if ($('.booking_legend').offset() != null) {
-      var top = $('.booking_legend').offset().top - parseFloat($('.booking_legend').css('marginTop').replace(/auto/, 0));
+    if ($('.rooms-current-search__info').offset() != null) {
+      var top = $('.rooms-current-search__info').offset().top - parseFloat($('.rooms-current-search__info').css('marginTop').replace(/auto/, 0));
       $(window).scroll(function (event) {
         // what the y position of the scroll is
         var y = $(this).scrollTop();
@@ -20,11 +20,11 @@
         if (y >= top) {
           // if so, ad the fixed class
           $('#rooms_booking_button').addClass('fixed');
-          $('.booking_legend').addClass('fixed');
+          $('.rooms-current-search__info').addClass('fixed');
         } else {
           // otherwise remove it
           $('#rooms_booking_button').removeClass('fixed');
-          $('.booking_legend').removeClass('fixed');
+          $('.rooms-current-search__info').removeClass('fixed');
         }
       });
     }
